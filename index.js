9cf0e9bde6572dbcb29d1617029fec3372a568a4
Le <andre.le@thomsonreuters.com>
2020-03-04 14:34:07 +1100

    dice 04.03.2020 add btn v2

--- index.js
+++ index.js
@@ -28,36 +28,40 @@ function rollDiceBtnClickEvent() {
     var randomNumber3 = getRandomInt(6);
     var points = randomNumber1 + randomNumber2 + randomNumber3;
     var winner = Math.max(randomNumber1, randomNumber2, randomNumber3);
-    reset++;
     //3 players outcome
-    if (randomNumber1 == winner) {
-        if (randomNumber2 == winner && randomNumber2 > randomNumber3) { outcome = "Player 1 and 2 win!!"; } else if (randomNumber3 == winner && randomNumber3 > randomNumber2) { outcome = "Player 1 and 3 win!!"; } else { outcome = "Player 1 win!!"; }
-    } else if (randomNumber2 == winner) {
-        if (randomNumber1 == winner && randomNumber1 > randomNumber3) { outcome = "Player 1 and 2 win!!"; } else if (randomNumber3 == winner && randomNumber3 > randomNumber1) { outcome = "Player 2 and 3 win!!"; } else { outcome = "Player 2 win!!"; }
-    } else if (randomNumber3 == winner) {
-        if (randomNumber2 == winner && randomNumber2 > randomNumber1) { outcome = "Player 2 and 3 win!!"; } else if (randomNumber1 == winner && randomNumber1 > randomNumber2) { outcome = "Player 1 and 3 win!!"; } else { outcome = "Player 3 win!!"; }
-    } else if (randomNumber1 == winner &&
-        randomNumber2 == winner &&
-        randomNumber3 == winner) {
-        outcome = "It's a draw!!";
-    }
-    //assign value to imgsrc variable
-    var imgsrc1 = "images\\dice" + randomNumber1 + ".png";
-    var imgsrc2 = "images\\dice" + randomNumber2 + ".png";
-    var imgsrc3 = "images\\dice" + randomNumber3 + ".png";
+    if (reset == 2) { window.location.reload(false); } else {
+        reset++;
+        if (randomNumber1 == winner) {
+            if (randomNumber2 == winner && randomNumber2 > randomNumber3) { outcome = "Player 1 and 2 win!!"; } else if (randomNumber3 == winner && randomNumber3 > randomNumber2) { outcome = "Player 1 and 3 win!!"; } else { outcome = "Player 1 win!!"; }
+        } else if (randomNumber2 == winner) {
+            if (randomNumber1 == winner && randomNumber1 > randomNumber3) { outcome = "Player 1 and 2 win!!"; } else if (randomNumber3 == winner && randomNumber3 > randomNumber1) { outcome = "Player 2 and 3 win!!"; } else { outcome = "Player 2 win!!"; }
+        } else if (randomNumber3 == winner) {
+            if (randomNumber2 == winner && randomNumber2 > randomNumber1) { outcome = "Player 2 and 3 win!!"; } else if (randomNumber1 == winner && randomNumber1 > randomNumber2) { outcome = "Player 1 and 3 win!!"; } else { outcome = "Player 3 win!!"; }
+        } else if (randomNumber1 == winner &&
+            randomNumber2 == winner &&
+            randomNumber3 == winner) {
+            outcome = "It's a draw!!";
+        }
+        //assign value to imgsrc variable
+        var imgsrc1 = "images\\dice" + randomNumber1 + ".png";
+        var imgsrc2 = "images\\dice" + randomNumber2 + ".png";
+        var imgsrc3 = "images\\dice" + randomNumber3 + ".png";
 
-    //change the image source of img tags
-    document.querySelector(".img1").setAttribute("src", imgsrc1);
-    document.querySelector(".img2").setAttribute("src", imgsrc2);
-    document.querySelector(".img3").setAttribute("src", imgsrc3);
-    if (reset != 0) {
-        document.querySelector("h2").textContent = outcome;
+        //change the image source of img tags
+        document.querySelector(".img1").setAttribute("src", imgsrc1);
+        document.querySelector(".img2").setAttribute("src", imgsrc2);
+        document.querySelector(".img3").setAttribute("src", imgsrc3);
+        if (reset != 0 && outcome != "It's a draw!!") {
+            document.querySelector("h2").textContent = outcome;
 
-        //Fire up the confetti
-        //document.querySelector(".confetti").setAttribute("visibility", "visible");
-        document.querySelector(".confetti").classList.toggle("visible"); //replace toggle with add/remove
+            //Fire up the confetti
+            //document.querySelector(".confetti").setAttribute("visibility", "visible");
+            document.querySelector(".confetti").classList.add("visible"); //replace toggle with add/remove
+        }
+
+        document.querySelector(".rollbutton").innerHTML = "Let roll again";
+        reset++;
     }
-    reset = 0
 
 
 }
